dc980290afe1dfaee191c138798a35774735a9b0
Alexander 'ccntrq' Pankoff <ccntrq@screenri.de>
2023-08-26 18:02:13 +0200

    Add 'map' and 'source' fields to Game

--- src/Battlesnake/Core/Game.hs
+++ src/Battlesnake/Core/Game.hs
@@ -16,8 +16,14 @@ data Game = Game
   -- ^ The ID of the game.
   , ruleset :: Ruleset
   -- ^ The ruleset used in the game.
+  , map :: Text
+  -- ^ The name of the map used to create the game. (See
+  -- https://docs.battlesnake.com/api/objects/game-maps for possible values.)
   , timeout :: Integer
-  -- ^ The maximum time in milliseconds the server will wait for a response from your server.
+  -- ^ The maximum time in milliseconds the server will wait for a response from
+  -- your server.
+  , source :: Text
+  -- ^ The source of the game.
   }
   deriving (Show, Eq, Generic)
 
